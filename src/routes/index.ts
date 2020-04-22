f5fe32457dabbd242d47a8af0a0241056cb384ba
miroswd <altamirsantos93@hotmail.com>
2020-04-22 19:40:39 -0300

    Padronizaçao de codigo

---
+++ src/routes/index.ts
@@ -0,0 +1,18 @@
+// Main route file
+
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
+});
+
+export default routes;
