2d4f43cf0ded18eca337ee9758445b446427c9e3
miroswd <altamirsantos93@hotmail.com>
2020-04-30 19:56:45 -0300

    Model, Repositories and Routes

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,44 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointments';
+
+// DTO = Data Transfer Object
+
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  // LISTAGEM
+  public all(): Appointment[] {
+    return this.appointments;
+  }
+
+  // POST
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+    // funciona como se fosse um else
+    return findAppointment || null;
+  }
+
+  // O retorno vai ser um objeto do mesmo formato de appointment
+  // public create(provider: string, date: Date): Appointment {
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
