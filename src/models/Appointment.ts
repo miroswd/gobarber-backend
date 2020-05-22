13b45c54e629df620c0a018b6b5f4e1292763896
miroswd <altamirsantos93@hotmail.com>
2020-05-22 18:53:34 -0300

    Migrations

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,17 +1,24 @@
-import { uuid } from 'uuidv4';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'; // entidade: algo q vai ser salvo no banco de dados
 
+// PrimaryGeneratedColumn -> para gerar um id
+
+@Entity('appointments') // Passando a classe como parâmentro para a entidade
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column() // por padrão é varchar
   provider: string;
 
+  @Column('time with time zone')
   date: Date;
 
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
+  /* Removendo o constructor, pois será inicializado através do typeorm */
+  // constructor({ provider, date }: Omit<Appointment, 'id'>) {
+  //   this.id = uuid();
+  //   this.provider = provider;
+  //   this.date = date;
+  // }
 }
 
 export default Appointment;
