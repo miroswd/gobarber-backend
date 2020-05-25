926638fd7174db3ab7e0d9f9688e70b98d425476
miroswd <altamirsantos93@hotmail.com>
2020-05-25 20:55:37 -0300

    Tratativa de erros

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import User from '../models/User';
+import AppError from '../errors/AppError';
 import authConfig from '../config/auth';
 
 interface Request {
@@ -20,7 +21,7 @@ class AuthenticateUserService {
     const user = await userRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw new Error('Incorrect email/password combination');
+      throw new AppError('Incorrect email/password combination', 401);
     }
 
     // user.password == senha criptografada
@@ -30,7 +31,7 @@ class AuthenticateUserService {
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error('Incorrect email/password combination');
+      throw new AppError('Incorrect email/password combination', 401);
     }
     const { secret, expiresIn } = authConfig.jwt;
 
