340c5c59859c48e3f2782a03bf941e89832417c0
Altamir Santos <54915150+miroswd@users.noreply.github.com>
2020-05-25 21:34:21 -0300

    Tratativa de erros

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,7 +5,6 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 sessionsRouter.post('/', async (request, response) => {
   const { email, password } = request.body;
-  console.log(request.body);
   const authenticateUserService = new AuthenticateUserService();
   const { user, token } = await authenticateUserService.execute({
     email,
