97a8cff0e78958ee60fffdbf001d9cdccacbdb70
miroswd <altamirsantos93@hotmail.com>
2020-05-25 21:32:32 -0300

    Insomnia

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,6 +5,7 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 sessionsRouter.post('/', async (request, response) => {
   const { email, password } = request.body;
+  console.log(request.body);
   const authenticateUserService = new AuthenticateUserService();
   const { user, token } = await authenticateUserService.execute({
     email,
