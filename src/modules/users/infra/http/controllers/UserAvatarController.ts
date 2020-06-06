141d0d4c41b91a2f5fb42799655ee0c781f49071
miroswd <altamirsantos93@hotmail.com>
2020-06-06 20:41:57 -0300

    Incluindo controllers

---
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -0,0 +1,19 @@
+// Controller deve ter apenas os métodos do CRUD
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+
+export default class UsersAvatarController {
+  async update(request: Request, response: Response): Promise<Response> {
+    // Editando apenas uma informação
+    const updateUserAvatarService = container.resolve(UpdateUserAvatarService);
+
+    const user = await updateUserAvatarService.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+
+    delete user.password;
+    return response.json(user);
+  }
+}
