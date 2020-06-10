453e11193c84bf939c85a71e0b01beb0f16d4307
miroswd <altamirsantos93@hotmail.com>
2020-06-10 13:28:52 -0300

    Envio de e-mails

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,11 +1,25 @@
 import { container } from 'tsyringe';
 
-// import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import EtherealMailProvider from '@shared/container/providers/MailProvider/implementations/EtherealMailProvider';
 
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
+import IMailTemplateProvider from './MailtTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvide from './MailtTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
+);
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvide,
+);
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  container.resolve(EtherealMailProvider),
 );
