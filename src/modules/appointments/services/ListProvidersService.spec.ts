144a73a387da58bfd263a2391078821b14681a3e
miroswd <altamirsantos93@hotmail.com>
2020-06-18 13:26:54 -0300

    List Providers

---
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,39 @@
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProviderService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProviders: ListProvidersService;
+
+describe('ListProviders', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    listProviders = new ListProvidersService(fakeUsersRepository);
+  });
+
+  it('should be able to list the providers', async () => {
+    const user1 = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const user2 = await fakeUsersRepository.create({
+      name: 'John Tre',
+      email: 'johntre@example.com',
+      password: '123456',
+    });
+
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'John Four',
+      email: 'johnfour@example.com',
+      password: '123456',
+    });
+
+    const providers = await listProviders.execute({
+      user_id: loggedUser.id,
+    });
+
+    await expect(providers).toEqual([user1, user2]);
+  });
+});
